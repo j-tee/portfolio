1a657382cb8c90e050fcf85b566960145a401c32
j-tee <juliustetteh@gmail.com>
2023-03-18 19:28:31 +0000

    update index/css and home.js
    Signed-off-by: j-tee <juliustetteh@gmail.com>

--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import Navbar from './Navbar';
+import { Carousel } from 'react-bootstrap';
 
 function Home() {
   return (
@@ -10,14 +11,39 @@ function Home() {
         <Row>
           <Col md={6}>
             <div className="hero-text">
-              <h1>Welcome to my portfolio</h1>
-              <p>I am a React web developer with experience in building beautiful and responsive web applications.</p>
+              <h1>Hello, My name is Tetteh Julius (TeeJay)</h1>
+              <p>I am a Fullstack web developer and a graduate of Microverse with experience in building beautiful and responsive web applications.</p>
+
               <button className="btn btn-primary">View Projects</button>
             </div>
           </Col>
           <Col md={6}>
             <div className="hero-image">
-              <img src="https://scontent.facc9-1.fna.fbcdn.net/v/t1.6435-9/54212925_2176028732440617_8148463789931495424_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=19026a&_nc_eui2=AeE5cF92PlfjU4Hzq4grPMZEsWVtBQvfMt-xZW0FC98y3wgQMG9TSM5hlMfU4X51jHSB6gKlC_g3NXT2FtpOHTzm&_nc_ohc=7btoQdnAaCIAX-RQZ3K&_nc_ht=scontent.facc9-1.fna&oh=00_AfB8F85y80KlHEEaJbbklKBAf9cA-JPRV0ymqfUqdPfVSQ&oe=64386678" alt="hero" />
+              <Carousel className="carousel-container">
+                <Carousel.Item className='item'>
+                  <p>
+                    My experience at Microverse has greatly improved my front-end development skills.
+                    I worked on a number of exercises including weekly projects that were focused on front-end technologies such as CSS, javascript, and HTML.
+                    I have had experience in other front-end technologies such as react and bootstrap.
+                  </p>
+                </Carousel.Item>
+                <Carousel.Item className='item'>
+                  <p>
+                    The Microverse experience is a hands-on student-centered program. This approach helped me cultivate relevant
+                    skills in researching and learning new concepts.
+                    As an online school, learning requires remote collaboration with other students across the globe on various
+                    projects. This experience has improved my collaborative and remote working skills,
+                  </p>
+                </Carousel.Item>
+                <Carousel.Item className='item'>
+                  <p>
+                    My four-year bachelor’s program has been mentally stimulating and provided me with the academic disciplines and
+                    the resilience to
+                    complete assignments on schedule and according to specific requirements. Having been an active footballer
+                    in the past, the experiences of defeat and victory have thought me how to deal with odds and uphill tasks.
+                  </p>
+                </Carousel.Item>
+              </Carousel>
             </div>
           </Col>
         </Row>
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { store } from './app/redux/store';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -14,9 +13,7 @@ const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <App />
   </React.StrictMode>
 );
 
